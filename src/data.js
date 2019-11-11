04c353a889a14441af264d17e8aa349b78a1aa92
jennlindley <jennifersu2059@gmail.com>
2019-11-11 11:37:03 -0500

    filtrado varitas

--- src/data.js
+++ src/data.js
@@ -1,8 +1,90 @@
-/* Manejo de data */
+// Filtrado de patronus
+export const filterPatronus = (data, prop) => {
+    let arrayPatronus = [];
+    for (let i = 0; i < data.length; i += 1) {
+      if (data[i][prop]) {
+        arrayPatronus.push({
+          name: data[i].name,
+          image: data[i].image,
+          patronus: data[i].patronus
+        });
+      }
+    }
+    return arrayPatronus;
+  };
 
-// esta es una función de ejemplo
+  export const imagePatronus = (data) => {
+    let newPatronus = '';
+    for (let i = 0; i < data.length; i += 1 ) {
+      newPatronus += `
+        <section class = "vista4">
+          <img class ="imagen3" src=${data[i].image}>
+          <p class ="patronus"> Nombre: ${data[i].patronus} </p>
+          <p class ="nombre2"> Pertenece a: ${data[i].name} </p>
+        </section>
+      `;
+    }
+    return newPatronus;
+  }
 
-export const verpersonajes = () => 'verpersonajes';
-// for (let i = 0; i < POTTER.length; i += 1) {
-//  containerImage(POTTER[i]);
-// }
+  export const filterVaritas = (data, propiedad) => {
+    let arrayVaritas = [];
+    for (let i = 0; i < data.length; i += 1) {
+      if (data[i][propiedad]) {
+        arrayVaritas.push({
+          name: data[i].name,
+          image: data[i].image,
+          wand: {
+            wood: data[i].wand.wood,
+            core: data[i].wand.core,
+          }
+        });
+      }
+    }
+    return arrayVaritas;
+  };
+
+  export const imageVaritas = (data) => {
+    let newVaritas = '';
+    for (let i = 0; i < data.length; i += 1 ) {
+      newVaritas += `
+        <section class="vista3">
+          <img class="imagen4" src=${data[i].image}>
+          <p class="nombre3"> Madera: ${data[i].wand.wood} </p>
+          <p class="nombre3"> Núcleo: ${data[i].wand.core} </p>
+          <p class="nombre3"> Pertenece a: ${data[i].name} </p>
+        
+        </section>
+      `;
+    }
+    return newVaritas;
+  }
+
+  export const filterRoles = (data, propiedad, value) => {
+    const newRoles = [];
+    for (let i = 0; i < data.length; i += 1) {
+      if (data[i][propiedad] === value) {
+        newRoles.push({
+          name: data[i].name,
+          image: data[i].image,
+        });
+      }
+    }
+    return newRoles;
+  };
+
+  export const filterCasas = (data, propiedad, value) => {
+    const newCasas = [];
+    for (let i = 0; i < data.length; i += 1) {
+      if (data[i][propiedad] === value) {
+        newCasas.push({
+          name: data[i].name,
+          image: data[i].image,
+        });
+      }
+    }
+    return newCasas;
+  };
+
+  
+
--- src/main.js
+++ src/main.js
@@ -1,51 +1,94 @@
 import POTTER from './data/potter/potter.js';
-import { verpersonajes } from './data.js';
+import {filterPatronus, imagePatronus, filterVaritas, imageVaritas, filterRoles, filterCasas} from './data.js';
+
+
 
 // Declaracion de Variables
 const vista2 = document.querySelector('#vista2');
 const datosPersonaje = document.querySelector('#datosPersonaje');
 const vista1 = document.querySelector('#vista1');
 const btnpersonajes = document.querySelector('#btnpersonajes');
-const varitas = document.querySelector('#btnvaritas');
+const btnVaritas = document.querySelector('#btnvaritas');
 const patronum = document.querySelector('#btnpatronum');
 const vista3 = document.querySelector('#vista3');
 const vista4 = document.querySelector('#vista4');
+const roles = document.querySelector('#roles');
+const casas = document.querySelector('#casas');
 
+// Nueva sección creada para contener la data de personajes y sus caracteristicas
 const containerImage = (data) => {
   const sectionElement = document.createElement('section');
   sectionElement.innerHTML = `
-  <img class = "imagen" src= ${data.image}>
-  <p class ="nombre"> ${data.name} </p>
+  <img class = "imagen1" src= ${data.image}>
+  <p class ="nombre1"> Nombre: ${data.name} </p>
   `;
   sectionElement.addEventListener('click', (event) => {
     event.preventDefault();
     vista2.classList.add('hide');
     datosPersonaje.classList.remove('hide');
-    datosPersonaje.innerHTML = `
-
+    datosPersonaje.innerHTML = `  
     <ul class="cartilla" >
-    <img class = "imagen" src= ${data.image}>
-    <li>Name: ${data.name}</li>
-    <li>Specie: ${data.species}</li>
-    <li>Gender: ${data.gender}</li>
-    <li>House: ${data.house}</li>   
-    <li>Date of Birth: ${data.dateOfBirth}</li>  
-    <li>Year of Birth: ${data.yearOfBirth}</li>    
-    <li>Ancestry: ${data.ancestry}</li>    
-    <li>Eye Colour: ${data.eyeColour}</li>   
-    <li>Hair Colour: ${data.hairColour}</li>
+    <img class = "imagen2" src= ${data.image}>
+    <li>Nombre: ${data.name}</li>
+    <li>Especie: ${data.species}</li>
+    <li>Género: ${data.gender}</li>
+    <li>Casa: ${data.house}</li>   
+    <li>Fecha de Nacimiento: ${data.dateOfBirth}</li>  
+    <li>Año de Nacimiento: ${data.yearOfBirth}</li>    
+    <li>Ascendencia: ${data.ancestry}</li>    
+    <li>Color de Ojos: ${data.eyeColour}</li>   
+    <li>Color de Cabello: ${data.hairColour}</li>
     <li>Patronus: ${data.patronus}</li>   
     <li>Actor: ${data.actor}</li>    
     </ul>
     `;
   });
 
-  vista2.appendChild(sectionElement);
+  return sectionElement;
 };
 
+// Select de roles
 for (let i = 0; i < POTTER.length; i += 1) {
-  containerImage(POTTER[i]);
+  vista2.appendChild(containerImage(POTTER[i]));
 }
+
+roles.addEventListener('change', (event) => {
+  vista2.innerHTML = '';
+
+  if (event.target.value === 'estudiantes') {
+    const filterEstudiantes = filterRoles(POTTER, 'hogwartsStudent', true);
+    for (let i = 0; i < filterEstudiantes.length; i += 1) {
+      vista2.appendChild(containerImage(filterEstudiantes[i]));
+    }
+  } else if (event.target.value === 'profesores') {
+    const filterProfesores = filterRoles(POTTER, 'hogwartsStaff', true);
+    for (let i = 0; i < filterProfesores.length; i += 1) {
+      vista2.appendChild(containerImage(filterProfesores[i]));
+    }
+  }
+});
+
+// Select de casas
+for (let i = 0; i < POTTER.length; i += 1) {
+  vista2.appendChild(containerImage(POTTER[i]));
+}
+
+casas.addEventListener('change', (event) => {
+  vista2.innerHTML = '';
+
+  if (event.target.value === 'gryffindor') {
+    const filterCasas1 = filterCasas(POTTER, 'house', 'gryffindor');
+    for (let i = 0; i < filterCasas1.length; i += 1) {
+      vista2.appendChild(containerImage(filterCasas1[i]));
+    }
+  } else if (event.target.value === 'slytherin') {
+    const filterCasas1 = filterCasas(POTTER, 'house', 'slytherin');
+    for (let i = 0; i < filterCasas1.length; i += 1) {
+      vista2.appendChild(containerImage(filterCasas1[i]));
+    }
+  }
+});
+
 
 // Función ocultar para boton personajes
 
@@ -55,20 +98,20 @@ btnpersonajes.addEventListener('click', (event) => {
   vista2.classList.remove('hide');
 });
 
-// Funcion ocultar para boton varitas
-varitas.addEventListener('click', (event) => {
+// Funcion ocultar para boton varitas y mostrar datos de las varitas
+btnVaritas.addEventListener('click', (event) => {
   event.preventDefault();
   vista1.classList.add('hide');
   vista3.classList.remove('hide');
+  const filterVaritas1 = filterVaritas(POTTER,'wand');
+  vista3.innerHTML= imageVaritas(filterVaritas1);
 });
 
-// Funcion ocultar para boton patronum
+// Funcion ocultar para boton patronum y jalar datos de patronus
 patronum.addEventListener('click', (event) => {
   event.preventDefault();
   vista1.classList.add('hide');
   vista4.classList.remove('hide');
+  const filterPatronus1 = filterPatronus(POTTER, 'patronus');
+  vista4.innerHTML= imagePatronus(filterPatronus1);
 });
-
-/*
-console.log(POTTER)
-*/
